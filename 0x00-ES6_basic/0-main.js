01fa339b01de217ff9d219ee4cfc068eeed4be0c
Catercodes <chrisadamsadewuni@gmail.com>
2024-05-30 06:43:03 +0100

    const and let

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,15 +1,3 @@
-export function taskFirst() {
-    var task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+import { taskFirst, taskNext } from './0-constants.js';
+
+console.log(`${taskFirst()} ${taskNext()}`);
