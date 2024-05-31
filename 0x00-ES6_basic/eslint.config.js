43897ea4ad7ad8c857afb5b3251d1141c3be68de
Catercodes <chrisadamsadewuni@gmail.com>
2024-05-31 22:56:37 +0100

    JavaScript

--- 0x00-ES6_basic/eslint.config.js
+++ 0x00-ES6_basic/eslint.config.js
@@ -1,8 +1,24 @@
-import globals from "globals";
+// Import necessary plugins and configurations
 import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+// Export the ESLint configuration
+export default {
+  extends: [
+    "next/core-web-vitals", // Use Next.js recommended config
+    "eslint:recommended", // Use ESLint's recommended config
+    "plugin:@typescript-eslint/recommended" // Use TypeScript's recommended config
+  ],
+  parser: "@typescript-eslint/parser", // Use TypeScript parser
+  plugins: ["@typescript-eslint"], // Enable TypeScript plugin
+  languageOptions: {
+    globals: {
+      ...pluginJs.configs.recommended.languageOptions.globals,
+      ...tseslint.configs.recommended.languageOptions.globals,
+      browser: true // Add additional global if needed
+    }
+  },
+  rules: {
+    // Add custom rules if needed
+  }
+};
