46c9c245eb56a93a52e173af158f9194b5218bef
Catercodes <chrisadamsadewuni@gmail.com>
2024-06-22 23:33:46 +0100

    Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,13 @@
+const keyIncome = 'income';
+const keyGdp = 'gdp';
+const keyCapita = 'capita';
+
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    [keyIncome]: income,
+    [keyGdp]: gdp,
+    [keyCapita]: capita,
+  };
 
   return budget;
 }
